4ec55c5d4d90ae2996f2e4c57c909776655c8fac
agnesuxa <agnygury@yahoo.com>
2024-10-26 19:35:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // script.js
 
 const allEpisodes = [
-  { name: "Episode 1: Haunted Tales", img: "https://anibox.lol/images/episode1.jpg", url: "https://anibox.lol/episode/1" },
-  { name: "Episode 2: Witch’s Curse", img: "https://anibox.lol/images/episode2.jpg", url: "https://anibox.lol/episode/2" },
-  { name: "Episode 3: Vampire’s Night", img: "https://anibox.lol/images/episode3.jpg", url: "https://anibox.lol/episode/3" },
-  { name: "Episode 4: Ghostly Encounters", img: "https://anibox.lol/images/episode4.jpg", url: "https://anibox.lol/episode/4" },
-  { name: "Episode 5: Dark Forest", img: "https://anibox.lol/images/episode5.jpg", url: "https://anibox.lol/episode/5" },
-  { name: "Episode 6: Demon’s Wrath", img: "https://anibox.lol/images/episode6.jpg", url: "https://anibox.lol/episode/6" },
-  { name: "Episode 7: Sorcerer’s Secrets", img: "https://anibox.lol/images/episode7.jpg", url: "https://anibox.lol/episode/7" },
-  { name: "Episode 8: Midnight Horror", img: "https://anibox.lol/images/episode8.jpg", url: "https://anibox.lol/episode/8" },
-  { name: "Episode 9: Spellbound", img: "https://anibox.lol/images/episode9.jpg", url: "https://anibox.lol/episode/9" },
-  { name: "Episode 10: Cursed Village", img: "https://anibox.lol/images/episode10.jpg", url: "https://anibox.lol/episode/10" },
+  { name: "Episode 1: Bătaie cu Bunica Turbo", img: "https://anibox.lol/wp-content/uploads/2024/10/eTbwdeKsf7MV3Lj73F1VRQHfx80-2.jpg", url: "https://anibox.lol/watch/dandadan-episoade-4/" },
+  { name: "Episode 2: Asasinul și Ninja-ul", img: "https://anibox.lol/wp-content/uploads/2024/10/97NloZcN94eEMjCqdgtLwWwMs5-2.jpg", url: "https://anibox.lol/watch/blue-lock-vs-u-20-japan-episoade-26/" },
+  { name: "Episode 3: Festivalul Sanctuarului Amagami ~Conexiuni~ ", img: "https://anibox.lol/wp-content/uploads/2024/10/rWsVdKauv99X3CevDLCIgS71GiT-1.jpg", url: "https://anibox.lol/watch/amagami-san-chi-no-enmusubi-episoade-4/" },
+  { name: "Episode 4: Răufăcătoarea și Câmpul de Flori", img: "https://anibox.lol/wp-content/uploads/2024/10/u74UjSS69QikgtQ0GCeYXzGr7rq-1.jpg", url: "https://anibox.lol/watch/rekishi-ni-nokoru-akujo-ni-naru-zo-episoade-4/" },
+  { name: "Episode 5: Episodul 4", img: "https://anibox.lol/wp-content/uploads/2024/10/me5QUo9p1iP3fa6d0hMR8XTqUJ9-2-scaled.jpg", url: "https://anibox.lol/watch/uzumaki-episoade-4/" },
+  { name: "Episode 6: O scuză nu va fi suficientă", img: "https://anibox.lol/wp-content/uploads/2024/10/43HWZJ7tXSsaZZDZofAS1KZJJZW-3.jpg", url: "https://anibox.lol/watch/saikyou-no-shienshoku-wajutsushi-de-aru-ore-wa-sekai-saikyou-clan-wo-shitagaeru-episoade-4/" },
+  { name: "Episode 7: Masca misterioasă vrea să lupte", img: "https://anibox.lol/wp-content/uploads/2024/10/zHtApWbMP7U5gFgYxtUtVWN6lyR-2.jpg", url: "https://anibox.lol/watch/nageki-no-bourei-wa-intai-shitai-episoade-4/" },
+  { name: "Episode 8: În drum spre oraș", img: "https://anibox.lol/wp-content/uploads/2024/10/z0So3ih6jgnrLqJeLLEA72LaT9l-1.jpg", url: "https://anibox.lol/watch/hitoribocchi-no-isekai-kouryaku-episoade-4/" },
+  { name: "Episode 9: Este o confruntare Bunică vs. Bunică!", img: "https://anibox.lol/wp-content/uploads/2024/10/mM7WAhNZyXO0axLZ0FGhNIwY5Xv-2.jpg", url: "https://anibox.lol/watch/dandadan-episoade-3/" },
+  { name: "Episode 10: O întâlnire cu zeii", img: "https://anibox.lol/wp-content/uploads/2024/10/zylq3FNjQxziseJBvhyKzutGc2f-2.jpg", url: "https://anibox.lol/watch/amagami-san-chi-no-enmusubi-episoade-3/" },
   // Add more episodes here
 ];
 
